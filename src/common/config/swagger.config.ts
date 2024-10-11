53cb0a4f1da0cf3ec62a68547a461b8f0774b946
Alisker <alisker.babaev@pay-go.ru>
2024-10-11 07:05:57 +0300

    swagger

---
+++ src/common/config/swagger.config.ts
@@ -0,0 +1,7 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+	.setTitle('Stud Progress API')
+	.setVersion('1.0')
+	.addTag('SP')
+	.build();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { swaggerConfig } from './common/config/swagger.config';
 
 async function bootstrap() {
-	const port = process.env.port ?? 4200;
-
+	const PORT = process.env.port ?? 4200;
 	const app = await NestFactory.create(AppModule);
-	await app.listen(port);
+
+	const document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
