a7cb0bdf55573f86d16f117379d2bee6b4f9970e
alisker <balisker@mail.ru>
2025-05-25 13:41:14 +0300

    access token expiration enable

--- src/models/token/jwt.strategy.ts
+++ src/models/token/jwt.strategy.ts
@@ -9,7 +9,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	constructor(private configService: ConfigService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: configService.get(JWT_SECRET_KEY)
+			secretOrKey: configService.get(JWT_SECRET_KEY),
+			ignoreExpiration: false
 		});
 	}
 
