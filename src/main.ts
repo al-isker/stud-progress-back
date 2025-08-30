455e60bef5628fbdbd688d3c3b7369a8f32876d8
alisker <balisker@mail.ru>
2025-08-30 03:14:01 +0300

    https enable

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { swaggerConfig } from './common/config/swagger.config';
+import { httpsOptions } from './common/config/https-options';
+import { swaggerConfig } from './common/config/swagger-config';
 
 async function bootstrap() {
 	const PORT = process.env.port ?? 4200;
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { httpsOptions });
 
 	const document = SwaggerModule.createDocument(app, swaggerConfig);
 	SwaggerModule.setup('api', app, document);
