0fb73775c405dd10c5192580deeeeb6220a55075
Alisker <alisker.babaev@pay-go.ru>
2026-02-17 04:49:50 +0300

    transform full name in login dto

--- src/models/auth/dto/login.dto.ts
+++ src/models/auth/dto/login.dto.ts
@@ -1,9 +1,11 @@
 import { IsInt, IsString, Max, Min } from 'class-validator';
+import { FullNameCase } from 'src/common/lib/transformers/full-name-case';
 import { Trim } from 'src/common/lib/transformers/trim';
 
 export class LoginDto {
 	@IsString()
 	@Trim()
+	@FullNameCase()
 	fullName: string;
 
 	@IsString()
