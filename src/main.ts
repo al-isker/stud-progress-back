1a7161efec692e36641ad28ba79808445f65259f
alisker <balisker@mail.ru>
2026-03-25 00:36:35 +0300

    correct port environment key

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './common/config/swagger/swagger-config';
+import { PORT_KEY } from './common/lib/env/env-keys';
 
 async function bootstrap() {
-	const PORT = process.env.port ?? 4200;
+	const PORT = process.env[PORT_KEY] ?? 4200;
 	const app = await NestFactory.create(AppModule);
 
 	const document = SwaggerModule.createDocument(app, swaggerConfig);
