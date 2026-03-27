bb503eefd4394e2b68e4c7a2df140d9f08301a28
alisker <balisker@mail.ru>
2026-03-28 02:49:41 +0300

    status code 200 for all post

--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CurrentStudentId } from '../student/decorators/current-student-id.decorator';
 import { AuthService } from './auth.service';
 import { Auth } from './decorators/auth.decorator';
@@ -10,16 +10,19 @@ export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post('login')
+	@HttpCode(HttpStatus.OK)
 	login(@Body() dto: LoginDto) {
 		return this.authService.login(dto);
 	}
 
 	@Post('refresh-token')
+	@HttpCode(HttpStatus.OK)
 	refreshToken(@Body() dto: RefreshTokenDto) {
 		return this.authService.refreshToken(dto);
 	}
 
 	@Post('logout')
+	@HttpCode(HttpStatus.OK)
 	@Auth()
 	logout(@CurrentStudentId() studentId: number) {
 		return this.authService.logout(studentId);
--- src/models/device-info/device-info.controller.ts
+++ src/models/device-info/device-info.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { CurrentStudentId } from '../student/decorators/current-student-id.decorator';
 import { DeviceInfoService } from './device-info.service';
@@ -9,6 +9,7 @@ export class DeviceInfoController {
 	constructor(private readonly deviceInfoService: DeviceInfoService) {}
 
 	@Post('expo-push-token')
+	@HttpCode(HttpStatus.OK)
 	@Auth()
 	updateExpoPushToken(@CurrentStudentId() studentId: number, @Body() dto: UpdateExpoPushTokenDto) {
 		return this.deviceInfoService.updateExpoPushToken(studentId, dto);
